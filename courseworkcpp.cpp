f9d45adec1ff1d4f2b69fe118aa039a27efb6913
NullExistence <poticnijd6@gmail.com>
2026-01-30 12:16:50 +0200

    Add files via upload

---
+++ courseworkcpp.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cstring>     
+#include <cstddef>     
+
+class StringRow
+{
+private:
+    char* data;                  
+    size_t max_length;           
+
+public:
+    StringRow()
+        : data(nullptr), max_length(0)
+    {
+        data = new char[1];
+        data[0] = '\0';
+    }
+
+    explicit StringRow(size_t maxLen)
+        : max_length(maxLen)
+    {
+        data = new char[maxLen + 1];
+        data[0] = '\0';
+    }
+
+    ~StringRow()
+    {
+        delete[] data;
+    }
+
+    StringRow(const StringRow&) = delete;
+    StringRow& operator=(const StringRow&) = delete;
+    StringRow(StringRow&&) = delete;
+    StringRow& operator=(StringRow&&) = delete;
+};
+
+int main() {
+    return 0;
+}
